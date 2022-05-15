1f8e73be4da8677108bbd25f90bf5497a4c01c46
minhtuan1407 <m.tuan9a2@gmail.com>
2022-05-15 11:42:31 +0700

    change: fix name common tags

--- Day-12/networking/resources.tf
+++ Day-12/networking/resources.tf
@@ -54,7 +54,7 @@ data "consul_keys" "networking" {
 locals {
   cidr_block   = jsondecode(data.consul_keys.networking.var.networking)["cidr_block"]
   subnet_count = jsondecode(data.consul_keys.networking.var.networking)["subnet_count"]
-  common_tags = merge(jsondecode(data.consul_keys.networking.var.common-tags),
+  common_tags = merge(jsondecode(data.consul_keys.networking.var.common_tags),
     {
       Environment = terraform.workspace
     }
